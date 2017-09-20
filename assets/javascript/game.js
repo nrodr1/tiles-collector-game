ff2d87c6aad6a14c1810a2e93020722c87b2450e
Noel P. Rodriguez <noel.p.rodriguez@gmail.com>
2017-09-20 15:59:51 -0500

    jQuery Code
    Adding initial jQuery code for game to work.

---
+++ assets/javascript/game.js
@@ -0,0 +1,59 @@
+$( document ).ready(function(){
+  var Random=Math.floor(Math.random()*101+19)
+
+
+// Select a random number at the start of the game
+
+  $('#randomNumber').text(Random);
+
+// Appending random number to the randomNumber id in the html doc
+
+  var num1= Math.floor(Math.random()*11+1)
+  var num2= Math.floor(Math.random()*11+1)
+  var num3= Math.floor(Math.random()*11+1)
+  var num4= Math.floor(Math.random()*11+1)
+
+// Random numbers for each tile, random number between 1 - 12
+
+  var userTotal= 0; 
+  var wins= 0;
+  var losses = 0;
+
+//  Decare variables for scores
+
+$('#numberWins').text(wins);
+$('#numberLosses').text(losses);
+
+//Reset the game function
+
+function reset(){
+      Random=Math.floor(Math.random()*101+19);
+      console.log(Random)
+      $('#randomNumber').text(Random);
+      num1= Math.floor(Math.random()*11+1);
+      num2= Math.floor(Math.random()*11+1);
+      num3= Math.floor(Math.random()*11+1);
+      num4= Math.floor(Math.random()*11+1);
+      userTotal= 0;
+      $('#finalTotal').text(userTotal);
+      }
+
+
+//Add wins to the userTotal
+
+function yay(){
+alert("Woot! Woot! You won!");
+  wins++; 
+  $('#numberWins').text(wins);
+  reset();
+}
+
+
+//Add losses to the userTotal
+
+function loser(){
+alert ("Bummer! You lost!");
+  losses++;
+  $('#numberLosses').text(losses);
+  reset()
+}
